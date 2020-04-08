64e0f164e8a0db3636376c988bec578d80a5fae0
anaxiel-code <lms.pereiraa@gmail.com>
2020-04-07 21:08:01 -0300

    Adicionando um arquivo

---
+++ Teste.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+import javax.swing.JOptionPane;
+public class Teste {
+    public static void main(String[] args ) {
+        System.out.println("Teste");
+        JOptionPane.showMessageDialog(null, "Ola", "ola",3);
+    }
+}

cbe28aae5e1b38351bc9947421422f246872565c
anaxiel-code <lms.pereiraa@gmail.com>
2020-04-08 11:10:37 -0300

    mais um dia apos o outro

---
+++ teste2.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class teste
+{
+    public static void main (String[] args){
+        System.out.println("Olá mundo");
+        JOptionPane.showMessageDialog(null, "Testando o git", "teste", 2);
+    }
+}
